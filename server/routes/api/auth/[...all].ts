614a21e99d02fd847b33d366cfcb0b1c299c587c
curioschurro99-dot <curioschurro99@gmail.com>
2026-07-08 15:21:01 +0800

    fix: manually construct standard Request from h3 event for Better Auth

--- server/routes/api/auth/[...all].ts
+++ server/routes/api/auth/[...all].ts
@@ -1,4 +1,15 @@
-import { defineEventHandler, toRequest } from "h3";
+import { defineEventHandler, readRawBody } from "h3";
 import { auth } from "@/lib/auth-server";
 
-export default defineEventHandler((event) => auth.handler(toRequest(event)));
+export default defineEventHandler(async (event) => {
+  const host = event.headers.get("host") ?? "localhost";
+  const proto = event.headers.get("x-forwarded-proto") ?? "http";
+  const url = `${proto}://${host}${event.path}`;
+  const rawBody = event.method !== "GET" && event.method !== "HEAD" ? await readRawBody(event) : undefined;
+  const req = new Request(url, {
+    method: event.method,
+    headers: event.headers,
+    body: rawBody,
+  });
+  return auth.handler(req);
+});
