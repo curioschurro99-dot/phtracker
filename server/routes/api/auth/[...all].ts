a245c1160fbd858e5cda4e30a1fbd2acc6d5ddeb
curioschurro99-dot <curioschurro99@gmail.com>
2026-07-08 15:16:53 +0800

    fix: use toRequest(event) for Better Auth handler instead of toWebRequest

--- server/routes/api/auth/[...all].ts
+++ server/routes/api/auth/[...all].ts
@@ -1,4 +1,4 @@
-import { defineEventHandler, toWebRequest } from "h3";
+import { defineEventHandler, toRequest } from "h3";
 import { auth } from "@/lib/auth-server";
 
-export default defineEventHandler((event) => auth.handler(toWebRequest(event)));
+export default defineEventHandler((event) => auth.handler(toRequest(event)));
